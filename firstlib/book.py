f6d808304d81a017ced564ed367e88007b46b791
Schwifter <d.goglidze@sangu.edu.ge>
2023-10-18 17:59:31 +0400

    WIP

--- firstlib/book.py
+++ firstlib/book.py
@@ -1,2 +1,32 @@
-def create_book(name: str, author: str) -> str:
-    return f"{name}: {author}"
+from dataclasses import dataclass
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@dataclass
+class Book(BaseModel):
+    title: str
+    author: str
+
+
+shelf = []
+
+
+@app.post("/books", status_code=201)
+def create_book(book: Book):
+    book = {
+        "title": book.title,
+        "author": book.author,
+    }
+
+    shelf.append(book)
+
+    return book
+
+
+@app.get("/books", status_code=200)
+def show_shelf():
+    return {"shelf": shelf}
--- tests/unit/test_book.py
+++ tests/unit/test_book.py
@@ -1,5 +1,46 @@
-from firstlib.book import create_book
+from fastapi.testclient import TestClient
+
+from firstlib.book import app, shelf
+
+client = TestClient(app)
 
 
-def test_create_book() -> None:
-    assert create_book("LOTR", "Tolkien") == "LOTR: Tolkien"
+def test_create_book():
+    response = client.post(
+        "/books",
+        json={
+            "title": "LOTR",
+            "author": "Tolkien",
+        },
+    )
+
+    assert response.status_code == 201, response.json()
+    assert response.json() == {
+        "title": "LOTR",
+        "author": "Tolkien",
+    }
+
+    shelf.clear()
+
+
+def test_read_book():
+    client.post(
+        "/books",
+        json={
+            "title": "LOTR",
+            "author": "Tolkien",
+        },
+    )
+    response = client.get("/books")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "shelf": [
+            {
+                "title": "LOTR",
+                "author": "Tolkien",
+            }
+        ]
+    }
+
+    shelf.clear()
