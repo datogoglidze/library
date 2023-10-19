fd8e0daa057a7a4c775cb1ba8214f103ada2e860
Schwifter <d.goglidze@sangu.edu.ge>
2023-10-19 16:17:03 +0400

    Add necessary fields for book

--- firstlib/api/book.py
+++ firstlib/api/book.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Any
+from uuid import UUID
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -8,18 +10,28 @@ app = FastAPI()
 
 @dataclass
 class Book(BaseModel):
+    book_id: UUID
     title: str
     author: str
+    isbn: str
+    publisher: str
+    total_pages: int
+    year: int
 
 
 shelf = []
 
 
 @app.post("/books", status_code=201)
-def create_book(book: Book) -> dict[str, str]:
+def create_book(book: Book) -> dict[str, Any]:
     book_info = {
+        "book_id": book.book_id,
         "title": book.title,
         "author": book.author,
+        "isbn": book.isbn,
+        "publisher": book.publisher,
+        "total_pages": book.total_pages,
+        "year": book.year,
     }
 
     shelf.append(book_info)
@@ -28,5 +40,5 @@ def create_book(book: Book) -> dict[str, str]:
 
 
 @app.get("/books", status_code=200)
-def show_shelf() -> dict[str, list[dict[str, str]]]:
+def show_shelf() -> dict[str, list[dict[str, Any]]]:
     return {"shelf": shelf}
--- tests/unit/api/test_book.py
+++ tests/unit/api/test_book.py
@@ -1,30 +1,37 @@
+from uuid import uuid4
+from faker import Faker
+
 from fastapi.testclient import TestClient
 
 from firstlib.api.book import app, shelf
 
 client = TestClient(app)
 
-
-book = {
-    "title": "LOTR",
-    "author": "Tolkien",
+book_info = {
+    "book_id": Faker().uuid4(),
+    "title": str(Faker().job()),
+    "author": str(Faker().first_name()),
+    "isbn": str(Faker().isbn13()),
+    "publisher": str(Faker().company()),
+    "total_pages": int(Faker().random_digit_not_null() * 10),
+    "year": int(Faker().year()),
 }
 
 
 def test_create_book() -> None:
-    response = client.post("/books", json=book)
+    response = client.post("/books", json=book_info)
 
     assert response.status_code == 201, response.json()
-    assert response.json() == book
+    assert response.json() == book_info
 
     shelf.clear()
 
 
 def test_read_book() -> None:
-    client.post("/books", json=book)
+    client.post("/books", json=book_info)
     response = client.get("/books")
 
     assert response.status_code == 200
-    assert response.json() == {"shelf": [book]}
+    assert response.json() == {"shelf": [book_info]}
 
     shelf.clear()
