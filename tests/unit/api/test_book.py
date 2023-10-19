76429d26b74640bd03772f6cd64c82a3393a4800
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2023-10-19 16:25:17 +0400

    Add more fields to book (#8)

--- tests/unit/api/test_book.py
+++ tests/unit/api/test_book.py
@@ -1,46 +1,30 @@
+from faker import Faker
 from fastapi.testclient import TestClient
 
-from firstlib.api.book import app, shelf
+from firstlib.api.book import app
 
 client = TestClient(app)
 
+book_info = {
+    "book_id": Faker().uuid4(),
+    "title": str(Faker().job()),
+    "author": str(Faker().first_name()),
+    "isbn": str(Faker().isbn13()),
+    "publisher": str(Faker().company()),
+    "total_pages": int(Faker().random_digit_not_null() * 10),
+    "year": int(Faker().year()),
+}
+
 
 def test_create_book() -> None:
-    response = client.post(
-        "/books",
-        json={
-            "title": "LOTR",
-            "author": "Tolkien",
-        },
-    )
+    response = client.post("/books", json=book_info)
 
     assert response.status_code == 201, response.json()
-    assert response.json() == {
-        "title": "LOTR",
-        "author": "Tolkien",
-    }
-
-    shelf.clear()
+    assert response.json() == book_info
 
 
 def test_read_book() -> None:
-    client.post(
-        "/books",
-        json={
-            "title": "LOTR",
-            "author": "Tolkien",
-        },
-    )
     response = client.get("/books")
 
     assert response.status_code == 200
-    assert response.json() == {
-        "shelf": [
-            {
-                "title": "LOTR",
-                "author": "Tolkien",
-            }
-        ]
-    }
-
-    shelf.clear()
+    assert response.json() == {"shelf": [book_info]}
