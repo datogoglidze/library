980493465ae2ede4ca3c8f296fa5ac7f1909bad7
Schwifter <d.goglidze@sangu.edu.ge>
2023-10-19 15:14:19 +0400

    Introduce variable

--- tests/unit/api/test_book.py
+++ tests/unit/api/test_book.py
@@ -5,42 +5,26 @@ from firstlib.api.book import app, shelf
 client = TestClient(app)
 
 
+book = {
+    "title": "LOTR",
+    "author": "Tolkien",
+}
+
+
 def test_create_book() -> None:
-    response = client.post(
-        "/books",
-        json={
-            "title": "LOTR",
-            "author": "Tolkien",
-        },
-    )
+    response = client.post("/books", json=book)
 
     assert response.status_code == 201, response.json()
-    assert response.json() == {
-        "title": "LOTR",
-        "author": "Tolkien",
-    }
+    assert response.json() == book
 
     shelf.clear()
 
 
 def test_read_book() -> None:
-    client.post(
-        "/books",
-        json={
-            "title": "LOTR",
-            "author": "Tolkien",
-        },
-    )
+    client.post("/books", json=book)
     response = client.get("/books")
 
     assert response.status_code == 200
-    assert response.json() == {
-        "shelf": [
-            {
-                "title": "LOTR",
-                "author": "Tolkien",
-            }
-        ]
-    }
+    assert response.json() == {"shelf": [book]}
 
     shelf.clear()
