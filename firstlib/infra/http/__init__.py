b012c72a355bf68fce4091e8f6b892553a725528
Schwifter <d.goglidze@sangu.edu.ge>
2023-10-25 17:32:07 +0400

    standartize api response

---
+++ firstlib/infra/http/__init__.py
@@ -0,0 +1,10 @@
+from .fluent import FluentHttp
+from .httpx import Httpx, HttpxConfig
+from .url import HttpUrl
+
+__all__ = [
+    "HttpUrl",
+    "Httpx",
+    "HttpxConfig",
+    "FluentHttp",
+]
