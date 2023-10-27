4e3246da5ae4112fb60ec8c5b4a364d85133f4d5
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2023-10-27 14:11:06 +0400

    Enhance authors (#28)

--- firstlib/infra/fastapi/authors.py
+++ firstlib/infra/fastapi/authors.py
@@ -1,15 +1,21 @@
 from typing import Any
 from uuid import UUID, uuid4
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 from firstlib.infra.fastapi.docs import Response
-from firstlib.infra.fastapi.response import ResourceCreated
+from firstlib.infra.fastapi.response import (
+    ResourceCreated,
+    ResourceExists,
+    ResourceFound,
+    ResourceNotFound,
+)
 
 authors_api = APIRouter(tags=["Authors"])
 
-authors: list[dict[str, Any]] = []
+all_authors: list[dict[str, Any]] = []
 
 
 class AuthorCreateRequest(BaseModel):
@@ -31,12 +37,17 @@ class AuthorItemEnvelope(BaseModel):
     author: AuthorItem
 
 
+class AuthorListEnvelope(BaseModel):
+    count: int
+    authors: list[AuthorItem]
+
+
 @authors_api.post(
     "",
     status_code=201,
     response_model=Response[AuthorItemEnvelope],
 )
-def create_author(request: AuthorCreateRequest) -> ResourceCreated:
+def create_author(request: AuthorCreateRequest) -> JSONResponse | dict[str, Any]:
     author = {
         "id": uuid4(),
         "name": request.name,
@@ -45,10 +56,34 @@ def create_author(request: AuthorCreateRequest) -> ResourceCreated:
         "bio": request.bio,
     }
 
-    for each_author in authors:
-        if each_author["name"] == author["name"]:
-            raise HTTPException(status_code=409, detail="Author already exists")
+    for author_info in all_authors:
+        if author_info["name"] == author["name"]:
+            return ResourceExists(
+                f"Author with name<{author_info['name']}> already exists."
+            )
 
-    authors.append(author)
+    all_authors.append(author)
 
     return ResourceCreated(author=author)
+
+
+@authors_api.get(
+    "",
+    status_code=200,
+    response_model=Response[AuthorListEnvelope],
+)
+def read_all() -> ResourceFound:
+    return ResourceFound(authors=all_authors, count=len(all_authors))
+
+
+@authors_api.get(
+    "/{author_id}",
+    status_code=200,
+    response_model=Response[AuthorItemEnvelope],
+)
+def read_one(author_id: UUID) -> ResourceFound | ResourceNotFound:
+    for author_info in all_authors:
+        if author_info["id"] == author_id:
+            return ResourceFound(author=author_info)
+
+    return ResourceNotFound(f"Author with id<{author_id}> does not exist.")
